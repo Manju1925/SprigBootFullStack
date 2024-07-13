11af6dad1828c4573fedfcc1ddf2b3e3e9b40c2f
Manju M S <manjumsbng2003@gmail.com>
2024-07-13 20:51:40 +0530

    Done

---
+++ src/main/java/com/manju/fullstack_backend/controllers/UserController.java
@@ -0,0 +1,49 @@
+package com.manju.fullstack_backend.controllers;
+
+import com.manju.fullstack_backend.exception.UserNotFoundException;
+import com.manju.fullstack_backend.model.User;
+import com.manju.fullstack_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setName(updatedUser.getName());
+                    user.setUsername(updatedUser.getUsername());
+                    user.setEmail(updatedUser.getEmail());
+                    return userRepository.save(user);
+                })
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/manju/fullstack_backend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.manju.fullstack_backend.repository;
+
+import com.manju.fullstack_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository <User,Long>{
+}
